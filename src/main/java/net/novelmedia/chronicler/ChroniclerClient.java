faeac32d5e55f1ee96a74be6e11b47d6019c9877
Paxton Weathers <paxton@drab.org>
2026-03-22 19:08:50 -0500

    Initial commit

---
+++ src/main/java/net/novelmedia/chronicler/ChroniclerClient.java
@@ -0,0 +1,11 @@
+package net.novelmedia.chronicler;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChroniclerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/novelmedia/chronicler/ChroniclerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.novelmedia.chronicler;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChroniclerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
